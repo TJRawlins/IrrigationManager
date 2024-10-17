c0c405c89c6b6c2e97a66f4b0ce7d61276f589fc
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-10-16 21:54:00 -0700

    Updated Zone ImagePath max string length from 200 to 500

---
+++ Migrations/20241014070001_Updated zone ImagePath max length.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IrrigationManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedzoneImagePathmaxlength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Zones",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Zones",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
