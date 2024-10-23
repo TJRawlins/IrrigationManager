af1164f39af75bc48d4b95129661174595d3587c
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-10-22 20:48:13 -0700

    Added GalsPerWkCalc property to Plant model. Updated default ImagePath value for Plant model.

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -11,13 +11,15 @@ namespace IrrigationManager.Models {
         public string Name { get; set;} = string.Empty;
         [Column(TypeName = "decimal(5,2)")]
         public decimal GalsPerWk { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal GalsPerWkCalc { get; set; }
         public int Quantity { get; set; }
         public int EmittersPerPlant { get; set; } = 1;
         [Column(TypeName = "decimal(5,2)")]
         public decimal EmitterGPH { get; set; }
         public string TimeStamp { get; set; } = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
         [StringLength(500)]
-        public string ImagePath { get; set; } = "https://t4.ftcdn.net/jpg/02/54/00/69/360_F_254006997_xRASPFxpBNlNiC4yFQxj8z8nrmFgVyNI.jpg";
+        public string ImagePath { get; set; } = "https://img.freepik.com/free-vector/light-gray-seamless-leaf-patterned-background-vector_53876-166104.jpg";
         public string Notes { get; set; } = string.Empty;
         public int Age { get; set; } = 0;
         public int HardinessZone { get; set; } = 1;
