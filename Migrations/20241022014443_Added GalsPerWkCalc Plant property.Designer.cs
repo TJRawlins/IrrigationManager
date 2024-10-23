ccd079887d708300f469d650336fdae16ec3512d
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-10-22 20:49:59 -0700

    Migrations for new GalsPerWkCalc property

--- Migrations/20241022014443_Added GalsPerWkCalc Plant property.Designer.cs
+++ Migrations/20241022014443_Added GalsPerWkCalc Plant property.Designer.cs
@@ -3,6 +3,7 @@ using IrrigationManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IrrigationManager.Migrations
 {
     [DbContext(typeof(IMSContext))]
-    partial class IMSContextModelSnapshot : ModelSnapshot
+    [Migration("20241022014443_Added GalsPerWkCalc Plant property")]
+    partial class AddedGalsPerWkCalcPlantproperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241022014443_Added GalsPerWkCalc Plant property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IrrigationManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGalsPerWkCalcPlantproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "GalsPerWkCalc",
+                table: "Plants",
+                type: "decimal(5,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GalsPerWkCalc",
+                table: "Plants");
+        }
+    }
+}
